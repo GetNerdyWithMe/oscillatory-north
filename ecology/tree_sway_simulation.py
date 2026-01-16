66b027338349af83dfee72dc3dbf58f67303d90b
Jennifer P. <98366370+GetNerdyWithMe@users.noreply.github.com>
2026-01-16 07:02:47 -0600

    Update tree_sway_simulation.py

--- ecology/tree_sway_simulation.py
+++ ecology/tree_sway_simulation.py
@@ -1,19 +1,15 @@
-# NOTE:
-# This file is a domain-specific application of the canonical oscillator
-# defined in src/oscillators.py
-
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
 def tree_sway(t, y, omega, zeta):
     x, x_dot = y
-    wind = 0.5 * np.sin(0.8 * t)
-    x_ddot = wind - 2*zeta*omega*x_dot - omega**2 * x
+    forcing = 0.5 * np.sin(0.8 * t)
+    x_ddot = forcing - 2*zeta*omega*x_dot - omega**2 * x
     return [x_dot, x_ddot]
 
-omega = 0.8   # effective sway frequency
-zeta = 0.2    # higher damping than swing
+omega = 0.8
+zeta = 0.2
 
 y0 = [0.0, 0.0]
 t_span = (0, 100)
@@ -30,6 +26,6 @@ solution = solve_ivp(
 plt.figure()
 plt.plot(solution.t, solution.y[0])
 plt.xlabel("Time")
-plt.ylabel("Lateral Displacement")
-plt.title("Tree Sway Under Oscillatory Wind")
+plt.ylabel("Displacement")
+plt.title("Tree sway")
 plt.show()
